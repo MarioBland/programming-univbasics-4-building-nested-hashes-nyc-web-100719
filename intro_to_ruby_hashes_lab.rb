1610a262b744c0e6f4f76677b336f79a337ae25c
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-09-16 01:20:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	 base_hash = { "key" => "thing"
-		 }
+  monopoly = { 
+			WHAT_KEY_HERE => {}
+		}
 end
 
 
