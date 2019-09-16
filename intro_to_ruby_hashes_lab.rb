62fc6fb03e512db50fcf922d1ce0ceea0441eff6
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-09-16 01:35:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,11 @@ def monopoly_with_third_tier
 	monopoly =  {
 		  :railroads => {
 				:pieces => 4,
-				:rent_in_dollars => {
-						:one_house => 100,
-						:two_house => 200,
-						:three_house => 300,
-						:hotel => 1000
+					:rent_in_dollars => {
+							:one_house => 100,
+							:two_house => 200,
+							:three_house => 300,
+							:hotel => 1000,
 					}
 				:names => {
 				}
